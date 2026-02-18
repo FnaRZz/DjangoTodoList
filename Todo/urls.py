92c46dce62010db31cb84427b15af5663880d2ac
FnaRZz <pidjakovdmitry@gmail.com>
2026-02-18 17:24:16 +0500

    Add task create

--- Todo/urls.py
+++ Todo/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "Todo"
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.task_list, name='home'),
+    path('create/', views.task_create, name='task_create'),
 ]
--- Todo/views.py
+++ Todo/views.py
@@ -1,11 +1,27 @@
 from .models import Task
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 
-def index(request):
+def task_list(request):
     tasks = Task.objects.all()
-    return render(request, 'index.html', {
+    return render(request, 'task_list.html', {
         'tasks': tasks,
     })
+
+def task_create(request):
+    if request.method == 'POST':
+        task_name = request.POST['task_name']
+        task_description = request.POST.get('task_description','')
+        task_completed = request.POST.get('task_completed') == 'on'
+        task_date = request.POST['task_date']
+        Task.objects.create(
+            title=task_name,
+            description=task_description,
+            completed=task_completed,
+            date=task_date
+        )
+        return redirect('Todo:home')
+    return render(request, 'create_task.html')
+
